3d878e6532ddc8d1e6888a2b54aa15890c88114d
Pawel Dera <paweldera01@gmail.com>
2022-03-13 13:18:59 +0100

    Added chart class

---
+++ src/main/java/ChartExample.java
@@ -0,0 +1,16 @@
+import org.jfree.chart.*;
+import org.jfree.ui.RefineryUtilities;
+
+public class ChartExample {
+    public static void main(String[] args) {
+
+       double x[] = {-1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0};
+       double y[] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0};
+
+
+        Chart chart = new Chart("ApplicationTitle", "ChartTitle", x, y);
+        chart.pack();
+        RefineryUtilities.centerFrameOnScreen(chart);
+        chart.setVisible(true);
+    }
+}
